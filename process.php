79c34bcc5bea1eac81e0366a96a23ec052911abf
Usman Abba Usman cbk <61090729+usmanabbacbk@users.noreply.github.com>
2021-07-07 04:39:08 +0100

    To add record of Isa as student

--- process.php
+++ process.php
@@ -4,6 +4,9 @@ echo '';
 $conn = mysqli_connect ("localhost", "isah", "123456", "isah");
 if($conn === false) {
     die ("Failed to connect to database");
+   
+$sql = "INSERT INTO student_record"(student_Name,student_Reg, student_Class,student_Age)
+VALUES('$varsname','$varsreg','varsclass','varsage')";
 }
 
 ?>
