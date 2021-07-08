5a17fbbf18ad243c46e42f145a1c04fc0eedfd5e
garoweb262 <muhdgazzali01@gmail.com>
2021-07-08 08:40:18 +0100

    maryam yushau

---
+++ procees1.php
@@ -0,0 +1,13 @@
+<title>footer</title>
+</head>
+<body>
+  <div>
+   <p> Reasons for Obtaining  data from student</p>
+  <li> This project is created to obtain  data of student from various faculty and save in a general database so as for easy acces in the future <br>
+  <li>Using Database management system, The Institution can have detailed information about student </li>
+  <li>With this Mini project of Epsilion Group  Records of students have been made easy to Access</li>
+ </li>
+  </div>
+  <br>
+</body>
+</html>
