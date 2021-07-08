c562dad4ae004b837146e368732292b674380155
ABDULGANIYU MUSTAPHA ABAYOMI <86324822+Mustysoft@users.noreply.github.com>
2021-07-08 09:49:58 +0100

    Update process.php

--- process.php
+++ process.php
@@ -4,9 +4,14 @@ echo '';
 $conn = mysqli_connect ("localhost", "isah", "123456", "isah");
 if($conn === false) {
     die ("Failed to connect to database");
+}
+
+$varsname = mysqli_real_escape_string($conn, $_REQUEST['sname']);
+$varsreg = mysqli_real_escape_string($conn, $_REQUEST['sreg']);
+$varsclass = mysqli_real_escape_string($conn, $_REQUEST['sclass']);
+$varsage = mysqli_real_escape_string($conn, $_REQUEST['sage']);
    
 $sql = "INSERT INTO student_record"(student_Name,student_Reg, student_Class,student_Age)
 VALUES('$varsname','$varsreg','varsclass','varsage')";
-}
 
 ?>
