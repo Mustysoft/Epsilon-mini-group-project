39d5d596ccf0dc0728676c6aea5515c2bab84572
ABDULGANIYU MUSTAPHA ABAYOMI <86324822+Mustysoft@users.noreply.github.com>
2021-07-16 10:22:48 +0100

    Update process.php

--- process.php
+++ process.php
@@ -14,6 +14,9 @@ $varsage = mysqli_real_escape_string($conn, $_REQUEST['sage']);
 $sql = "INSERT INTO student_record"(student_Name,student_Reg, student_Class,student_Age)
 VALUES('$varsname','$varsreg','varsclass','varsage')";
 
+echo  'Name :'.'&nbsp'.'Dear'.'&nbsp'. $_POST['sname'].'<br><br>'.'Reg number is:'. $_POST['sreg'].'<br><br>'.'Your level is: '.$_POST['sclass'].'<br><br>'.'Your age is: '.$_POST['sage'].'&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'<b><h3> We Have Received Your Information Which Have Been Submitted To Our Database!</h3><b>';
+
+
 echo '<html>
 <head>
 <title>
