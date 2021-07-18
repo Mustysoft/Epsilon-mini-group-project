adf8b250427600f95c3b7944d5964d5f275c0a70
Adebayo Abdulazeez Adeyinka <85970498+adekid018@users.noreply.github.com>
2021-07-18 14:43:06 +0100

    Update process.php

--- process.php
+++ process.php
@@ -18,11 +18,22 @@ echo  'Name :'.'&nbsp'.'Dear'.'&nbsp'. $_POST['sname'].'<br><br>'.'Reg number is
 
 
 echo '<html>
+
 <head>
 <title>
 </title>
 </head>
 <body>
+
+  <div>
+   <p> Reasons for Obtaining  data from student</p>
+  <li> This project is created to obtain  data of student from various faculty and save in a general database so as for easy acces in the future <br>
+  <li>Using Database management system, The Institution can have detailed information about student </li>
+  <li>With this Mini project of Epsilion Group  Records of students have been made easy to Access</li>
+ </li>
+  </div>
+  <br>
+
 </body>
 </html>
 ?>
